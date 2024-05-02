f46faf9148ee4ddb4a2f352736f197eb08312b9d
nguyentvit <122044845+nguyentvit@users.noreply.github.com>
2024-05-02 15:03:34 +0700

    sdf

---
+++ Web/static/js/MainPage.js
@@ -0,0 +1,176 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  // window.onscroll = function() {headerShadow()};
+  // function headerShadow() {
+  //   const navHeader =document.getElementById("header");
+  //   if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  //     navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+  //     navHeader.style.height = "70px";
+  //     navHeader.style.lineHeight = "70px";
+  //   } else {
+  //     navHeader.style.boxShadow = "none";
+  //     navHeader.style.height = "90px";
+  //     navHeader.style.lineHeight = "90px";
+  //   }
+  // }
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Employee Attendance Website"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+/* -- PROJECT BOX -- */
+sr.reveal('.explore-box',{interval: 200})
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* -- ABOUT INFO & CONTACT INFO -- & education */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+sr.reveal('.top-header.experiences',{})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+srLeft.reveal('.education-column.column1',{delay:400})
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+srRight.reveal('.education-column.column2',{delay:100})
+
+sr.reveal('.top-footer ',{})
+sr.reveal('footer ',{})
+sr.reveal('.footer-social-icons',{delayy: 200})
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+function validateForm() {
+  var emailInput = document.getElementById('myForm').elements['email'];
+  var emailError = document.getElementById('emailError');
+  var emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  if (!emailPattern.test(emailInput.value)) {
+      emailError.innerHTML = 'Invalid email format';
+      return false; // Prevent form submission
+  } else {
+      emailError.innerHTML = '';
+      return true; // Allow form submission
+  }
+}
+document.getElementById('icon_eye').addEventListener('click', function(e) {
+  var input = document.getElementById('dialog-password');
+  var eyeIcon = document.getElementById('icon_eye');
+  if(input.type=="password"){
+    input.type="text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  }else{
+    input.type="password";
+    eyeIcon.classList.add("fa-eye");
+    eyeIcon.classList.remove("fa-eye-slash");
+  }
+})
+
+const form = {
+  email: document.querySelector("#dialog-login-email"),
+  password: document.querySelector("#dialog-password")
+}
+
+async function getData(url = "", data)
+{
+  console.log(data)
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    mode: "cors",
+    body: JSON.stringify(data)
+  })
+  return response
+}
+
+document.getElementById('btn_close_dialog').addEventListener('click', function() {
+  document.getElementById('dialogOverlay').style.display = 'none';
+});
+document.addEventListener("DOMContentLoaded", function() {
+  // Ví dụ:
+  document.getElementById('login').addEventListener('click', function(event) {
+      event.preventDefault(); // Ngăn chặn hành động mặc định của nút
+      document.getElementById('dialogOverlay').style.display = 'block';
+  });
+
+});
+document.getElementById('btn-submit').addEventListener('click',(e) => {
+  e.preventDefault();
+  var body = {
+    "Email": form.email.value,
+    "Password": form.password.value
+  }
+  getData("https://localhost:7100/api/Account/login", body).then((data) => {
+    return data.json()
+  })
+  .then((data) => {
+    if (data.status == 200)
+    {
+      sessionStorage.setItem("token", data.token)
+      window.location.href = "../View/TimeSheetAdmin.html"
+    }
+    else {
+      console.log(data.errors)
+    }
+  })
+  
+  .catch((err) => {
+    console.log(err)
+  })
+} )
--- Web/static/js/TimeSheetAdmin.js
+++ Web/static/js/TimeSheetAdmin.js
@@ -5,3 +5,194 @@ $("input").on("change", function() {
           .format( this.getAttribute("data-date-format") )
   )
 }).trigger("change")
+
+
+function createTrTag(user) 
+{
+  // Tạo một thẻ <tr>
+  const row = document.createElement("tr");
+
+  // Tạo các cột dữ liệu cho user
+  const nameCell = document.createElement("td");
+  nameCell.className = "col_name";
+  const img = document.createElement("img");
+  img.src = "../Img/ava.jpg";
+  img.alt = "";
+  img.className = "img_ava";
+  const nameDiv = document.createElement("div");
+  nameDiv.className = "table_name_employee";
+  nameDiv.textContent = user.name; // Chỗ này bạn thay bằng dữ liệu tên của user
+  nameCell.appendChild(img);
+  nameCell.appendChild(nameDiv);
+
+  const departmentCell = document.createElement("td");
+  departmentCell.className = "col_depart";
+  departmentCell.textContent = user.department; // Chỗ này bạn thay bằng dữ liệu phòng ban của user
+
+  const checkInCell = document.createElement("td");
+  checkInCell.className = "col_depart";
+  checkInCell.textContent = user.checkInTime; // Chỗ này bạn thay bằng dữ liệu giờ check in của user
+
+  const checkOutCell = document.createElement("td");
+  checkOutCell.className = "col_depart";
+  checkOutCell.textContent = user.checkOutTime; // Chỗ này bạn thay bằng dữ liệu giờ check out của user
+
+  const statusCell = document.createElement("td");
+  statusCell.className = "col_depart";
+  statusCell.textContent = user.status; // Chỗ này bạn thay bằng dữ liệu trạng thái của user
+
+  // Thêm các cột dữ liệu vào thẻ <tr>
+  row.appendChild(nameCell);
+  row.appendChild(departmentCell);
+  row.appendChild(checkInCell);
+  row.appendChild(checkOutCell);
+  row.appendChild(statusCell);
+
+  return row;
+}
+
+const selectBox = document.querySelector(".department_options");
+const tbody = document.getElementById("tbody-users");
+async function getData(url = "", token)
+{
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    mode: "cors"
+  })
+  return response
+}
+
+const token = sessionStorage.getItem("token");
+
+getData("https://localhost:7100/api/Departments", token).then((data) => {
+  return data.json()
+})
+.then(departments => {
+  departments.forEach(department => {
+    const option = document.createElement("option");
+    option.value = department.departmentId
+    option.textContent = department.departmentName
+    selectBox.appendChild(option)
+
+  })
+}).catch((err) => {
+  console.log(err)
+})
+
+
+
+function loadDataTable() {
+  tbody.innerHTML = ""
+  var departmentName = ''
+  var departmentId = selectBox.value
+  if (departmentId != "department-All")
+  {
+    getData(`https://localhost:7100/api/Departments/${departmentId}`, token).then((data) => {
+  return data.json()
+})
+.then(department => {
+  departmentName = department.departmentName
+  return department.users
+})
+.then((users) => {
+  users.forEach(user => {
+    console.log(user)
+    var testuser = {
+      name: user.personName,
+  department: departmentName,
+  checkInTime: "6h55p",
+  checkOutTime: "11h20p",
+  status: "OnTime"
+    }
+    var userRow = createTrTag(testuser);
+    tbody.appendChild(userRow);
+  })
+}).catch((err) => {
+  console.log(err)
+})
+  }
+  else {
+    getData(`https://localhost:7100/api/Departments`, token).then((data) => {
+  return data.json()
+})
+.then(departments => {
+  departments.forEach(department => {
+    var dName = department.departmentName
+    department.users.forEach(user => {
+      var testuser = {
+        name: user.personName,
+    department: dName,
+    checkInTime: "6h55p",
+    checkOutTime: "11h20p",
+    status: "OnTime"
+      }
+      var userRow = createTrTag(testuser);
+      tbody.appendChild(userRow);
+    })
+  })
+}).catch((err) => {
+  console.log(err)
+})
+  }
+}
+
+selectBox.addEventListener("change", () => {
+  tbody.innerHTML = ""
+  var departmentName = ''
+  var departmentId = selectBox.value
+  if (departmentId != "department-All")
+  {
+    getData(`https://localhost:7100/api/Departments/${departmentId}`, token).then((data) => {
+  return data.json()
+})
+.then(department => {
+  departmentName = department.departmentName
+  return department.users
+})
+.then((users) => {
+  users.forEach(user => {
+    console.log(user)
+    var testuser = {
+      name: user.personName,
+  department: departmentName,
+  checkInTime: "6h55p",
+  checkOutTime: "11h20p",
+  status: "OnTime"
+    }
+    var userRow = createTrTag(testuser);
+    tbody.appendChild(userRow);
+  })
+}).catch((err) => {
+  console.log(err)
+})
+  }
+  else {
+    getData(`https://localhost:7100/api/Departments`, token).then((data) => {
+  return data.json()
+})
+.then(departments => {
+  departments.forEach(department => {
+    var dName = department.departmentName
+    department.users.forEach(user => {
+      var testuser = {
+        name: user.personName,
+    department: dName,
+    checkInTime: "6h55p",
+    checkOutTime: "11h20p",
+    status: "OnTime"
+      }
+      var userRow = createTrTag(testuser);
+      tbody.appendChild(userRow);
+    })
+  })
+}).catch((err) => {
+  console.log(err)
+})
+  }
+})
+
+document.addEventListener("DOMContentLoaded", loadDataTable())
